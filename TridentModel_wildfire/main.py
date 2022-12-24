ea2d037053a8c7efcf1b4434d71f77a9c4831747
fuwafuwaboom <ytx070109@gmail.com>
2022-12-24 12:53:37 -0600

    2022/12/24

---
+++ TridentModel_wildfire/main.py
@@ -0,0 +1,142 @@
+# License: BSD
+# Author: Sasank Chilamkurthy
+
+from __future__ import print_function, division
+import torchvision
+import torch
+import torch.nn as nn
+import sys
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+from torchvision import datasets, transforms
+import time
+import os
+import copy
+from model import *
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=15, freeze_epoch=-1):
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        # Freeze model
+        if epoch < freeze_epoch:
+            for parameter in model.base.parameters():
+                parameter.requires_grad = False
+        else:
+            for parameter in model.base.parameters():
+                parameter.requires_grad = True
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            count = 0
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                        print('Training Progress:{:.2f}%'.format(100 * count / dataset_sizes['train']),
+                              "-" * (10 * count / dataset_sizes['train']),
+                              end="")
+                        sys.stdout.flush()
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+                count += dataloaders['train'].batch_size
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+if __name__ == '__main__':
+    cudnn.benchmark = True
+    # plt.ion()  # interactive mode
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    data_dir = './ant_bee'
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=8,
+                                                  shuffle=True, num_workers=0)
+                   for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model_name = 'Efficientnet'
+
+    model = get_model(model_name)
+
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.Adam(model.parameters(), lr=0.0001)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=15,freeze_epoch=10)
+    torch.save(model.state_dict(), "./Storage_model" + str(model_name))
